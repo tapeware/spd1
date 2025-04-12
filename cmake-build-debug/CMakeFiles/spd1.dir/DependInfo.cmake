9f6f518adda8f265966cfec684fe92e35f8dc5cf
daniel <danielmalczyk2201@gmail.com>
2025-04-12 18:21:14 +0200

    korekcja div 2

---
+++ cmake-build-debug/CMakeFiles/spd1.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/danie/Documents/SPD/lab_spd/Heur.cpp" "CMakeFiles/spd1.dir/Heur.cpp.obj" "gcc" "CMakeFiles/spd1.dir/Heur.cpp.obj.d"
+  "C:/Users/danie/Documents/SPD/lab_spd/Problem.cpp" "CMakeFiles/spd1.dir/Problem.cpp.obj" "gcc" "CMakeFiles/spd1.dir/Problem.cpp.obj.d"
+  "C:/Users/danie/Documents/SPD/lab_spd/Schrage_alg.cpp" "CMakeFiles/spd1.dir/Schrage_alg.cpp.obj" "gcc" "CMakeFiles/spd1.dir/Schrage_alg.cpp.obj.d"
+  "C:/Users/danie/Documents/SPD/lab_spd/Solution.cpp" "CMakeFiles/spd1.dir/Solution.cpp.obj" "gcc" "CMakeFiles/spd1.dir/Solution.cpp.obj.d"
+  "C:/Users/danie/Documents/SPD/lab_spd/Task.cpp" "CMakeFiles/spd1.dir/Task.cpp.obj" "gcc" "CMakeFiles/spd1.dir/Task.cpp.obj.d"
+  "C:/Users/danie/Documents/SPD/lab_spd/algorithms.cpp" "CMakeFiles/spd1.dir/algorithms.cpp.obj" "gcc" "CMakeFiles/spd1.dir/algorithms.cpp.obj.d"
+  "C:/Users/danie/Documents/SPD/lab_spd/main.cpp" "CMakeFiles/spd1.dir/main.cpp.obj" "gcc" "CMakeFiles/spd1.dir/main.cpp.obj.d"
+  "C:/Users/danie/Documents/SPD/lab_spd/utils.cpp" "CMakeFiles/spd1.dir/utils.cpp.obj" "gcc" "CMakeFiles/spd1.dir/utils.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/spd1.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spd1.dir/Heur.cpp.obj"
+  "CMakeFiles/spd1.dir/Heur.cpp.obj.d"
+  "CMakeFiles/spd1.dir/Problem.cpp.obj"
+  "CMakeFiles/spd1.dir/Problem.cpp.obj.d"
+  "CMakeFiles/spd1.dir/Schrage_alg.cpp.obj"
+  "CMakeFiles/spd1.dir/Schrage_alg.cpp.obj.d"
+  "CMakeFiles/spd1.dir/Solution.cpp.obj"
+  "CMakeFiles/spd1.dir/Solution.cpp.obj.d"
+  "CMakeFiles/spd1.dir/Task.cpp.obj"
+  "CMakeFiles/spd1.dir/Task.cpp.obj.d"
+  "CMakeFiles/spd1.dir/algorithms.cpp.obj"
+  "CMakeFiles/spd1.dir/algorithms.cpp.obj.d"
+  "CMakeFiles/spd1.dir/main.cpp.obj"
+  "CMakeFiles/spd1.dir/main.cpp.obj.d"
+  "CMakeFiles/spd1.dir/utils.cpp.obj"
+  "CMakeFiles/spd1.dir/utils.cpp.obj.d"
+  "libspd1.dll.a"
+  "spd1.exe"
+  "spd1.exe.manifest"
+  "spd1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spd1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
